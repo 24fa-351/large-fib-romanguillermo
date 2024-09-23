aea57ae1c9e386661b4f5370dc363fc08e90705a
Guillermo Roman <roman.guillermo52@gmail.com>
2024-09-22 20:58:53 -0700

    submission

--- fib.c
+++ fib.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
-#include <stdint.h>
+#include <stdlib.h>
 
-int main(int argc, char* argv[]) {
-   printf("Hello!\n");
+// Recursive Fibonacci function
+long long int recursive(long long int n) {
+   if (n <= 1) return n;
+   return recursive(n - 1) + recursive(n - 2);
+}
+
+// Iterative Fibonacci function
+long long int iterative(long long int n) {
+   if (n <= 1) return n;
+   long long int prev = 0, curr = 1, next;
+   for (long long int i = 2; i <= n; i++) {
+      next = prev + curr;
+      prev = curr;
+      curr = next;
+   }
+      return curr;
+}
+
+long long int main(long long int argc, char *argv[]) {
+
+   // Variable Assignments
+   long long int firstInt = atoi(argv[1]);
+   char method = argv[2][0];
+
+   // Calculations
+   long long int N = firstInt - 1;
+
+   long long int fibNum;
+
+   if (method == 'r') {
+      fibNum = recursive(N);
+   } else if (method == 'i') {
+      fibNum = iterative(N);
+   }
+
+   // Output
+   printf("%lld\n", fibNum);
+   printf("The size of long long int is: %lu bytes\n", sizeof(long long int));
    return 0;
 }
+
